ecd30a08c5d041870c21beaa80651bd95361fb7a
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2022-08-11 03:55:14 +0200

    Fix NumberFormatException

--- src/main/java/de/brownie/coinsapi/commands/AddCoinsCMD.java
+++ src/main/java/de/brownie/coinsapi/commands/AddCoinsCMD.java
@@ -3,7 +3,6 @@ package de.brownie.coinsapi.commands;
 import de.brownie.coinsapi.CoinsAPIPlugin;
 import de.brownie.coinsapi.utils.ChatUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,28 +20,32 @@ public class AddCoinsCMD implements CommandExecutor {
                 if (args.length > 0) {
                     if (args.length == 1) {
                         String amount = args[0];
-                        if (Objects.nonNull(amount)) {
-                            try {
-                                CoinsAPIPlugin.INSTANCE.getCoinsAPI().addCoins(p.getName(), Integer.parseInt(amount));
-                            } catch (SQLException e) {
-                                throw new RuntimeException(e);
+                        try {
+                            if (Objects.nonNull(amount)) {
+                                try {
+                                    CoinsAPIPlugin.INSTANCE.getCoinsAPI().addCoins(p.getName(), Integer.parseInt(amount));
+                                    ChatUtils.sendMessage(p, String.format("&7You gave &byourself &e%s Cookies &7!", amount));
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e);
+                                }
                             }
-                            ChatUtils.sendMessage(p, String.format("&7You gave &byourself &e%s Coins &7!", amount));
+                        } catch (NumberFormatException e) {
+                            ChatUtils.sendMessage(p, "&cAmount was not a valid number!");
                         }
                     } else if (args.length == 2) {
                         String name = args[0];
                         String amount = args[1];
-                        if (Objects.nonNull(amount) && Objects.nonNull(name)) {
-                            try {
-                                CoinsAPIPlugin.INSTANCE.getCoinsAPI().addCoins(name, Integer.parseInt(amount));
-                            } catch (SQLException e) {
-                                throw new RuntimeException(e);
+                        try {
+                            if (Objects.nonNull(amount) && Objects.nonNull(name)) {
+                                try {
+                                    CoinsAPIPlugin.INSTANCE.getCoinsAPI().addCoins(name, Integer.parseInt(amount));
+                                    ChatUtils.sendMessage(p, String.format("&7You gave &e%s Cookies &7to &b%s&7!", amount, name));
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e);
+                                }
                             }
-                            ChatUtils.sendMessage(p, String.format("&7You gave &e%s &7Coins &b%s&7!", amount, name));
-                            Player target = Bukkit.getPlayer(name);
-                            if(Objects.nonNull(target)) {
-                                ChatUtils.sendMessage(target, "You have received &e" + amount + " &7Coins!");
-                            }
+                        } catch (NumberFormatException e) {
+                            ChatUtils.sendMessage(p, "&cAmount was not a valid number!");
                         }
                     } else {
                         ChatUtils.sendMessage(p, "&c/addcoins <amount>");
